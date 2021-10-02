27579588645c78df830d7be6378a67f1d4c757f9
Vitalii Isaenko <lookyisaenko@gmail.com>
2021-10-02 13:35:32 +0200

    Add header layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import logo from "../../images/logo.png";
+import { Link } from 'react-router-dom';
+import "./Header.css"
 
 function Header(props) {
     return (
       <header className="header">
         <img className="header__logo" src={logo} alt="логотип Место" />
-        <div className="header__nav">{props.children}</div>
+        <div className="header__nav">
+        <Link className="button button_type_register" to="/sign-up">Регистрация</Link>
+        <Link className="button button_type_login" to="/sign-in">Войти</Link>
+        </div>
       </header>
     );
   }
