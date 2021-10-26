49a4be4624df8a05fbab8e3f4dd73bcea6b81e20
Vitalii Isaenko <lookyisaenko@gmail.com>
2021-10-27 01:10:58 +0200

    Cleanup

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,11 +5,11 @@ function Portfolio() {
   return (
     <section className="portfolio">
       <h2 className="portfolio__header">Портфолио</h2>
-      <PortfolioLink name="Статичный сайт" link="https://www.google.com"></PortfolioLink>
+      <PortfolioLink name="Статичный сайт" link="https://github.com/VitaliiIsaenko/how-to-learn"/>
       <Delimiter/>
-      <PortfolioLink name="Адаптивный сайт" link="https://www.google.com"></PortfolioLink>
+      <PortfolioLink name="Адаптивный сайт" link="https://github.com/VitaliiIsaenko/russian-travel"/>
       <Delimiter/>
-      <PortfolioLink name="Одностраничное приложение" link="https://www.google.com"></PortfolioLink>
+      <PortfolioLink name="Одностраничное приложение" link="https://github.com/VitaliiIsaenko/react-mesto-api-full"/>
     </section>
   );
 }
